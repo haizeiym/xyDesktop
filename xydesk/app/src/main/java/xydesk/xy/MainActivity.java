70a3b015d0ce550124b0fc6f19f767aa3b6a7c34
haizeiym <1411164481@qq.com>
2016-08-25 22:12:57 +0800

    讯飞语音

--- xydesk/app/src/main/java/xydesk/xy/MainActivity.java
+++ xydesk/app/src/main/java/xydesk/xy/MainActivity.java
@@ -5,12 +5,28 @@ import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v4.app.FragmentActivity;
 import android.view.View;
 import android.widget.TextView;
 
+import com.iflytek.cloud.ErrorCode;
+import com.iflytek.cloud.InitListener;
+import com.iflytek.cloud.RecognizerListener;
+import com.iflytek.cloud.RecognizerResult;
+import com.iflytek.cloud.SpeechConstant;
+import com.iflytek.cloud.SpeechError;
+import com.iflytek.cloud.SpeechRecognizer;
+import com.iflytek.cloud.SpeechUtility;
+import com.iflytek.sunflower.FlowerCollector;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 
 import xydesk.xy.appAll.ui.AllAppShowUI;
 import xydesk.xy.contant.XYContant;
@@ -18,12 +34,18 @@ import xydesk.xy.db.DeskDB;
 import xydesk.xy.utils.AppUtils;
 import xydesk.xy.utils.Utils;
 import xydesk.xy.viewFragment.OneAppFragment;
+import xydesk.xy.voice.JsonParser;
 import xydesk.xy.xydesk.R;
 
 public class MainActivity extends FragmentActivity implements View.OnClickListener {
-    TextView all_app, xyCall;
+    TextView all_app, xyCall, add_three;
     OneAppFragment oneAppFragment;
+    // 语音听写对象
+    private SpeechRecognizer mIat;
+    int ret = 0; // 函数调用返回值
     public static MainActivity instance;
+    // 用HashMap存储听写结果
+    private HashMap<String, String> mIatResults = new LinkedHashMap<String, String>();
     public Handler handler = new Handler() {
         @Override
         public void handleMessage(Message msg) {
@@ -47,12 +69,28 @@ public class MainActivity extends FragmentActivity implements View.OnClickListen
         instance = this;
         all_app = (TextView) findViewById(R.id.all_app);
         xyCall = (TextView) findViewById(R.id.add_four);
+        add_three = (TextView) findViewById(R.id.add_three);
         xyCall.setOnClickListener(this);
         all_app.setOnClickListener(this);
+        add_three.setOnClickListener(this);
+        mIat = SpeechRecognizer.createRecognizer(instance, mInitListener);
         AppUtils.getInstance().getAllAppList(instance);
         AppUtils.getInstance().getAppU(instance);
         setDefaultFargment();
     }
+
+    /**
+     * 初始化监听器。
+     */
+    private InitListener mInitListener = new InitListener() {
+
+        @Override
+        public void onInit(int code) {
+            if (code != ErrorCode.SUCCESS) {
+                Utils.getInstance().toast(instance, "初始化失败，错误码：" + code);
+            }
+        }
+    };
 
     private void setDefaultFargment() {
         FragmentManager fm = getFragmentManager();
@@ -80,7 +118,110 @@ public class MainActivity extends FragmentActivity implements View.OnClickListen
                     Utils.getInstance().toast(instance, "暂未安装该应用，为您跳转至系统拨号界面");
                 }
                 break;
+            case R.id.add_three:
+                FlowerCollector.onEvent(instance, "iat_recognize");
+                mIatResults.clear();
+                // 设置参数
+                setParam();
+                ret = mIat.startListening(mRecognizerListener);
+                if (ret != ErrorCode.SUCCESS) {
+                    Utils.getInstance().toast(instance, "听写失败,错误码：" + ret);
+                }
+                break;
         }
+    }
+
+    public void setParam() {
+        // 清空参数
+        mIat.setParameter(SpeechConstant.PARAMS, null);
+        // 设置听写引擎
+        String mEngineType = SpeechConstant.TYPE_CLOUD;
+        mIat.setParameter(SpeechConstant.ENGINE_TYPE, mEngineType);
+        // 设置返回结果格式
+        mIat.setParameter(SpeechConstant.RESULT_TYPE, "json");
+        // 设置语言
+        mIat.setParameter(SpeechConstant.LANGUAGE, "zh_cn");
+        // 设置语言区域
+        mIat.setParameter(SpeechConstant.ACCENT, "mandarin");
+        // 设置语音前端点:静音超时时间，即用户多长时间不说话则当做超时处理
+        mIat.setParameter(SpeechConstant.VAD_BOS, "5000");
+        // 设置语音后端点:后端点静音检测时间，即用户停止说话多长时间内即认为不再输入， 自动停止录音
+        mIat.setParameter(SpeechConstant.VAD_EOS, "1800");
+        // 设置标点符号,设置为"0"返回结果无标点,设置为"1"返回结果有标点
+        mIat.setParameter(SpeechConstant.ASR_PTT, "0");
+        // 设置音频保存路径，保存音频格式支持pcm、wav，设置路径为sd卡请注意WRITE_EXTERNAL_STORAGE权限
+        // 注：AUDIO_FORMAT参数语记需要更新版本才能生效
+        mIat.setParameter(SpeechConstant.AUDIO_FORMAT, "wav");
+        mIat.setParameter(SpeechConstant.ASR_AUDIO_PATH,
+                Environment.getExternalStorageDirectory() + "/msc/iat.wav");
+    }
+
+    /**
+     * 听写监听器。
+     */
+    private RecognizerListener mRecognizerListener = new RecognizerListener() {
+
+        @Override
+        public void onBeginOfSpeech() {
+            // 此回调表示：sdk内部录音机已经准备好了，用户可以开始语音输入
+            // showTip("开始说话");
+        }
+
+        @Override
+        public void onError(SpeechError error) {
+            // Tips：
+            // 错误码：10118(您没有说话)，可能是录音机权限被禁，需要提示用户打开应用的录音权限。
+            // 如果使用本地功能（语记）需要提示用户开启语记的录音权限。
+        }
+
+        @Override
+        public void onEndOfSpeech() {
+            // 此回调表示：检测到了语音的尾端点，已经进入识别过程，不再接受语音输入
+            // showTip("结束说话");
+        }
+
+        @Override
+        public void onResult(RecognizerResult results, boolean isLast) {
+            printResult(results);
+
+            if (isLast) {
+                // TODO 最后的结果
+            }
+        }
+
+        @Override
+        public void onVolumeChanged(int volume, byte[] data) {
+            // showTip("当前正在说话，音量大小：" + volume);
+        }
+
+        @Override
+        public void onEvent(int eventType, int arg1, int arg2, Bundle obj) {
+            // 以下代码用于获取与云端的会话id，当业务出错时将会话id提供给技术支持人员，可用于查询会话日志，定位出错原因
+            // 若使用本地能力，会话id为null
+            // if (SpeechEvent.EVENT_SESSION_ID == eventType) {
+            // String sid = obj.getString(SpeechEvent.KEY_EVENT_SESSION_ID);
+            // Log.d(TAG, "session id =" + sid);
+            // }
+        }
+    };
+
+    private void printResult(RecognizerResult results) {
+        String text = JsonParser.parseIatResult(results.getResultString());
+
+        String sn = null;
+        // 读取json结果中的sn字段
+        try {
+            JSONObject resultJson = new JSONObject(results.getResultString());
+            sn = resultJson.optString("sn");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        mIatResults.put(sn, text);
+        StringBuilder resultBuffer = new StringBuilder();
+        for (String key : mIatResults.keySet()) {
+            resultBuffer.append(mIatResults.get(key));
+        }
+        add_three.setText(resultBuffer.toString());
     }
 
     @Override
@@ -89,5 +230,13 @@ public class MainActivity extends FragmentActivity implements View.OnClickListen
         if (requestCode == XYContant.DELETER_APP) {
             handler.sendEmptyMessage(XYContant.DELETER_APP);
         }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // 退出时释放连接
+        mIat.cancel();
+        mIat.destroy();
     }
 }
